1013378b69d5aa106295d8e3ce2e7c12751ef8c7
negi1223 <115639201+negi1223@users.noreply.github.com>
2022-10-27 22:53:57 +0900

    Create JavaSprict.js

---
+++ JavaSprict.js
@@ -0,0 +1,66 @@
+//スクロールした際の動きを関数でまとめる
+function PageTopAnime() {
+	var scroll = $(window).scrollTop();
+	if (scroll >= 200){//上から200pxスクロールしたら
+		$('#page-top').removeClass('DownMove');//#page-topについているDownMoveというクラス名を除く
+		$('#page-top').addClass('UpMove');//#page-topについているUpMoveというクラス名を付与
+	}else{
+		if($('#page-top').hasClass('UpMove')){//すでに#page-topにUpMoveというクラス名がついていたら
+			$('#page-top').removeClass('UpMove');//UpMoveというクラス名を除き
+			$('#page-top').addClass('DownMove');//DownMoveというクラス名を#page-topに付与
+		}
+	}
+}// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+	PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+	PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// #page-topをクリックした際の設定
+$('#page-top a').click(function () {
+    $('body,html').animate({
+        scrollTop: 0//ページトップまでスクロール
+    }, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
+    return false;//リンク自体の無効化
+});
+
+
+//ローディングのJava↓
+
+//テキストのカウントアップ+バーの設定
+var bar = new ProgressBar.Line(splash_text, {//id名を指定
+	easing: 'easeInOut',//アニメーション効果linear、easeIn、easeOut、easeInOutが指定可能
+	duration: 1000,//時間指定(1000＝1秒)
+	strokeWidth: 0.2,//進捗ゲージの太さ
+	color: '#555',//進捗ゲージのカラー
+	trailWidth: 0.2,//ゲージベースの線の太さ
+	trailColor: '#bbb',//ゲージベースの線のカラー
+	text: {//テキストの形状を直接指定				
+		style: {//天地中央に配置
+			position: 'absolute',
+			left: '50%',
+			top: '50%',
+			padding: '0',
+			margin: '-30px 0 0 0',//バーより上に配置
+			transform:'translate(-50%,-50%)',
+			'font-size':'1rem',
+			color: '#fff',
+		},
+		autoStyleContainer: false //自動付与のスタイルを切る
+	},
+	step: function(state, bar) {
+		bar.setText(Math.round(bar.value() * 100) + ' %'); //テキストの数値
+	}
+});
+
+//アニメーションスタート
+bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 なら100%まで描画します
+	$("#splash_text").fadeOut(10);//フェイドアウトでローディングテキストを削除
+	$(".loader_cover-up").addClass("coveranime");//カバーが上に上がるクラス追加
+	$(".loader_cover-down").addClass("coveranime");//カバーが下に下がるクラス追加
+	$("#splash").fadeOut();//#splashエリアをフェードアウト
+});
