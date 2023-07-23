90bb15824a322f4bdf9ec41eaed41deb15bea8a2
negi1223 <115639201+negi1223@users.noreply.github.com>
2023-07-23 12:06:11 +0900

    Update JavaSprict.js

--- JavaSprict.js
+++ JavaSprict.js
@@ -24,7 +24,7 @@ $(window).on('load', function () {
 $('#page-top a').click(function () {
     $('body,html').animate({
         scrollTop: 0//ページトップまでスクロール
-    }, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
+    }, 0);//ページトップスクロールの速さ。数字が大きいほど遅くなる
     return false;//リンク自体の無効化
 });
 
